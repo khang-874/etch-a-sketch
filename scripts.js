5710e3606a4bae3a34193b120783daf6fe5b0ce5
Khang Tran <tuankhangtran@cmail.carleton.ca>
2022-12-25 10:59:39 -0500

    Add feature, hold spacebar to draw

--- scripts.js
+++ scripts.js
@@ -7,18 +7,55 @@ let numberOfColumns = 16;
 padContainer.style = `grid-template-rows: repeat(${numberOfRows},1fr)`;
 padContainer.style = `grid-template-columns: repeat(${numberOfColumns}, 1fr)`;
 
+let navigatorBar = document.createElement("div");
+navigatorBar.setAttribute("id", "navigatorBar");
+
+let resetButton = document.createElement("div");
+resetButton.setAttribute("id", "resetButton");
+resetButton. innerText = "Reset";
+
+let blackColorButton = document.createElement("div");
+
+
+let instruction = document.createElement("div");
+instruction.innerText = "Hold Spacebar to draw";
+instruction.id = "instruction";
+navigatorBar.appendChild(instruction);
+navigatorBar.appendChild(resetButton);
+let play = false;
+        
+window.addEventListener('keydown', e => {
+    if(e.key == " ")
+        play = true;  
+});
+
+window.addEventListener('keyup', e =>{
+    play = false;
+});
+
+function reset(e){
+    let padList = padContainer.querySelectorAll("div");
+    for(const pad of padList)
+        pad.style.backgroundColor = "white";
+}
+resetButton.addEventListener("mousedown", reset);
+
 function addColorClass(e){
-    let currentDiv = e.target;
-    currentDiv.style = "background-color: black";
+    if(play){
+        let currentDiv = e.target;
+        currentDiv.style.backgroundColor = "black";
+    }
 
 }
 for(let i = 0; i < numberOfRows; ++i){
     for(let j = 0; j < numberOfColumns; ++j){
         let pad = document.createElement("div");
         pad.setAttribute("class", `pad`);
-        pad.addEventListener("mouseover", addColorClass)
+        pad.addEventListener("mouseover", addColorClass);
+        pad.addEventListener
         padContainer.appendChild(pad);
     }
 }
 
+container.appendChild(navigatorBar);
 container.appendChild(padContainer);
