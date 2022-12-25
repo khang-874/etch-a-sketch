30f73496fa33513d24f55768fa4a4dc2ced8bda7
Khang Tran <tuankhangtran@cmail.carleton.ca>
2022-12-25 10:21:25 -0500

    Add event listener for grid

--- scripts.js
+++ scripts.js
@@ -7,11 +7,16 @@ let numberOfColumns = 16;
 padContainer.style = `grid-template-rows: repeat(${numberOfRows},1fr)`;
 padContainer.style = `grid-template-columns: repeat(${numberOfColumns}, 1fr)`;
 
+function addColorClass(e){
+    let currentDiv = e.target;
+    currentDiv.style = "background-color: black";
 
+}
 for(let i = 0; i < numberOfRows; ++i){
     for(let j = 0; j < numberOfColumns; ++j){
         let pad = document.createElement("div");
         pad.setAttribute("class", `pad`);
+        pad.addEventListener("mouseover", addColorClass)
         padContainer.appendChild(pad);
     }
 }
