85765223db6c855bef2ccf80d820a329d9034bf4
Khang Tran <tuankhangtran@cmail.carleton.ca>
2022-12-25 12:03:57 -0500

    Add Random color feature

--- scripts.js
+++ scripts.js
@@ -1,33 +1,78 @@
 const container = document.querySelector("#container");
 
 let padContainer = document.createElement("div");
-padContainer.setAttribute("id", "padContainer");
 let numberOfRows = 16;
 let numberOfColumns = 16;
+const defaultColor = "#322F2B";
+let color = defaultColor;
+{
+padContainer.setAttribute("id", "padContainer");
 padContainer.style = `grid-template-rows: repeat(${numberOfRows},1fr)`;
 padContainer.style = `grid-template-columns: repeat(${numberOfColumns}, 1fr)`;
-
+}
 let navigatorBar = document.createElement("div");
-navigatorBar.setAttribute("id", "navigatorBar");
-
+let draw = false;
+let erase = false;
+let eraserButton = document.createElement("div");
 let resetButton = document.createElement("div");
+let basicColorButton = document.createElement("div");
+let randomColorButton = document.createElement("div");
+let choosen = false;
+{
+navigatorBar.setAttribute("id", "navigatorBar");
 resetButton.className = "navigatorButton"
 resetButton.innerText = "Reset";
 
-let draw = false;
-let erase = false;
-
-let eraserButton = document.createElement("div");
 eraserButton.className = "navigatorButton";
 eraserButton.innerText = "Eraser";
+
+basicColorButton.className = "navigatorButton";
+basicColorButton.innerText = "Basic Color";
+
+randomColorButton.className = "navigatorButton";
+randomColorButton.innerText = "Random Color";
+
+function chooseDiv(e){
+    let choosenDiv = e.target;
+    choosen = !choosen;
+
+    if(choosenDiv.classList.contains("navigatorSelected"))
+        choosenDiv.classList.remove("navigatorSelected");
+    else{
+        for(let div of navigatorBar.querySelectorAll('div'))
+            div.classList.remove("navigatorSelected");
+        choosenDiv.classList.add("navigatorSelected");
+    }
+}
 eraserButton.addEventListener('mousedown', e => {
+    chooseDiv(e);
     erase = !erase;
-    eraserButton.classList.toggle("navigatorSelected")
 });
 
+basicColorButton.addEventListener('mousedown', e => {
+    chooseDiv(e);
+    reset(e);
+    color = defaultColor;
+});
+
+randomColorButton.addEventListener('mousedown', e => {
+    chooseDiv(e);
+    reset(e);
+    color = '';
+    console.log("Change color");
+});
+
+resetButton.addEventListener("mousedown", e => {
+    erase = false;
+    for(let div of navigatorBar.querySelectorAll('div'))
+        div.classList.remove("navigatorSelected");
+})
+
+navigatorBar.appendChild(basicColorButton);
+navigatorBar.appendChild(randomColorButton);
 navigatorBar.appendChild(eraserButton);
 navigatorBar.appendChild(resetButton);
-
+}
 
         
 window.addEventListener('mousedown', e => {
@@ -46,11 +91,28 @@ function reset(e){
 }
 resetButton.addEventListener("mousedown", reset);
 
-let basicColor = "#322F2B";
-function addColorClass(e){
+let colorList = [
+    "#FAEBD7",
+    "#EBFAD7",
+    "#D7FAE1",
+    "#D7F5FA",
+    "#D7D7FA",
+    "#F5D7FA",
+    "#FAD7E1"
+];
+function getRandomColor(){
+    let random = Math.floor(Math.random() * colorList.length);
+    return colorList[random];
+}
+function addColor(e){
     let currentDiv = e.target;
-    if(draw)
-        currentDiv.style.backgroundColor = basicColor;
+    if(draw){
+        if(color != '')
+            currentDiv.style.backgroundColor = color;
+        else{
+            currentDiv.style.backgroundColor = getRandomColor();
+        }
+    }
     if(erase)
         currentDiv.style.backgroundColor = "white";
 }
@@ -58,7 +120,7 @@ for(let i = 0; i < numberOfRows; ++i){
     for(let j = 0; j < numberOfColumns; ++j){
         let pad = document.createElement("div");
         pad.setAttribute("class", `pad`);
-        pad.addEventListener("mouseover", addColorClass);
+        pad.addEventListener("mouseover", addColor);
         pad.addEventListener
         padContainer.appendChild(pad);
     }
